ffa79c2140e4ec3335f986884f4dc8037acd3a78
Corey Tilson <owensdaniel8@yahoo.com>
2022-04-07 19:22:23 +0100

    commit tree a740661f70f7a3b11428c81b0942c2d37f08fc47

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat (COCK) {
+cats.push(COCK)
+}
+function destructivelyPrependCat(DICK) {
+cats.unshift(DICK)
+}
+function destructivelyRemoveLastCat(boy) {
+cats.pop(boy)
+}
+function destructivelyRemoveFirstCat(girl) {
+cats.shift(girl)
+}
+function appendCat(David) {
+const kittens = [...cats]
+kittens.push(David)
+return kittens
+}
+function prependCat(jon) {
+const kitty = [...cats]
+kitty.unshift(jon)
+return kitty
+}
+function removeLastCat(corey) {
+const remove = [...cats]
+remove.pop(corey)
+return remove
+}
+function removeFirstCat(cool) {
+const jeff = [...cats]
+jeff.shift(cool)
+return jeff
+}
+
+
+
